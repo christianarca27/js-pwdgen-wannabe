3a585f045ceb978300a76f023b406eff73482339
christianarca27 <christian.arca.mail@gmail.com>
2023-02-21 12:52:34 +0100

    feat: inserito script richiesto

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let userFirstName = prompt("Quale è il tuo nome?");
+
+let userLastName = prompt("Quale è il tuo cognome?");
+
+let userFavColor = prompt("Quale è il tuo colore preferito?");
+
+document.writeln(userFirstName + userLastName + userFavColor + 17);
